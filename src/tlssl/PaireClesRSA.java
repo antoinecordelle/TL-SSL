8548593d02e46ae9de4950b2004131c75ff4751a
antoinecordelle <antoinecordelle@gmail.com>
2019-10-16 15:43:18 +0200

    Implement RSA

--- src/tlssl/PaireClesRSA.java
+++ src/tlssl/PaireClesRSA.java
@@ -1,21 +1,27 @@
 package tlssl;
 
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 
 public class PaireClesRSA {
     private KeyPair key;
 
     PaireClesRSA() {
-
+        SecureRandom rand = new SecureRandom();
+        KeyPairGenerator kpg = null;
+        try {
+            kpg = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        kpg.initialize(512, rand);
+        this.key = kpg.generateKeyPair();
     }
 
     public PublicKey Publique() {
-
+        return this.key.getPublic();
     }
 
     public PrivateKey Privee() {
-
+        return this.key.getPrivate();
     }
 }
