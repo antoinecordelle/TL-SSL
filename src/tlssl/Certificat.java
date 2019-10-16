1b1a5a40a9019a7c25ce69a25039730adc471a61
antoinecordelle <antoinecordelle@gmail.com>
2019-10-16 14:58:13 +0200

    Config IntelliJ project

--- src/tlssl/Certificat.java
+++ src/tlssl/Certificat.java
@@ -3,12 +3,13 @@ package tlssl;
 import java.math.BigInteger;
 import java.security.PublicKey;
 
+
 public class Certificat {
     static private BigInteger segnum = BigInteger.ZERO;
     public x509Certificate x509;
 
     Certificat(String nom, PaireClesRSA cle, int validityDays) {
-        //
+
     }
 
     public boolean verifCertif(PublicKey pubKey) {
