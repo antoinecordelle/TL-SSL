6dfcb4d67427e7ca629bccb1577adbd4633c56fb
Romain Poirot <romain.poirot@supelec.fr>
2019-10-16 14:41:13 +0100

    Fix x509 import

--- src/tlssl/Certificat.java
+++ src/tlssl/Certificat.java
@@ -2,11 +2,12 @@ package tlssl;
 
 import java.math.BigInteger;
 import java.security.PublicKey;
+import java.security.cert.X509Certificate;
 
 
 public class Certificat {
     static private BigInteger segnum = BigInteger.ZERO;
-    public x509Certificate x509;
+    public X509Certificate x509;
 
     Certificat(String nom, PaireClesRSA cle, int validityDays) {
 
