9a250e2e250ff4243ce43b75f8664b9bdeac0a9e
Romain Poirot <romain.poirot@supelec.fr>
2019-10-23 15:16:18 +0100

    Add sockets and simple command interface

--- src/tlssl/Equipement.java
+++ src/tlssl/Equipement.java
@@ -1,7 +1,12 @@
 package tlssl;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.PublicKey;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 
 public class Equipement {
@@ -12,11 +17,15 @@ public class Equipement {
     private ArrayList<Triplet> ca;
     private ArrayList<Triplet> da;
 
-    Equipement (String nom, int port) throws Exception {
+    private Socket socket = null;
+
+    Equipement (String nom, int port) {
         monNom = nom;
+        monPort = port;
         maCle = new PaireClesRSA();
         monCert = new Certificat(monNom, maCle.Privee(), maCle.Publique(), 365);
         ca = new ArrayList<Triplet>();
+
     }
 
     public void affichage_da() {
@@ -60,5 +69,57 @@ public class Equipement {
         Certificat cert = new Certificat(nom, maCle.Privee(), pubKey, 365);
         Triplet triplet = new Triplet(nom, pubKey, cert.getX509Certificate());
         ca.add(triplet);
+    }
+
+    public void startServer() {
+// starts server and waits for a connection
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(monPort);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for a client ...");
+
+            socket = serverSocket.accept();
+            System.out.println("Client accepted");
+
+            // takes input from the client socket
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            X509Certificate cert = (X509Certificate) objectInputStream.readObject();
+            System.out.println(cert);
+
+            System.out.println("Closing connection");
+
+            // close connection
+            socket.close();
+        }
+        catch(IOException | ClassNotFoundException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public void startClient(){
+        try {
+            // establish a connection
+            socket = new Socket("localhost", monPort);
+            System.out.println("Connected");
+
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            System.out.println("Sending messages to the ServerSocket");
+            objectOutputStream.writeObject(monCert.getX509Certificate());
+
+            // close the connection
+            socket.close();
+        }
+        catch(IOException | CertificateException i)
+        {
+            System.out.println(i);
+        }
+
     }
 }
--- src/tlssl/Main.java
+++ src/tlssl/Main.java
@@ -1,15 +1,41 @@
 package tlssl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Equipement equipement1 = new Equipement("Monolithe", 666);
-            Equipement equipement2 = new Equipement("Bla", 666);
-            equipement2.certify(equipement1);
-            equipement2.affichage_ca();
-        } catch (Exception e) {
-            e.printStackTrace();
+        Equipement equipement = new Equipement("Monolithe", 6666);
+
+        BufferedReader input  = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("Over"))
+        {
+            try
+            {
+                line = input.readLine();
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
+            }
+
+            switch (line) {
+                case "start server":
+                    equipement.startServer();
+                    break;
+
+                case "start client":
+                    equipement.startClient();
+                    break;
+
+                }
+
         }
 
     }
